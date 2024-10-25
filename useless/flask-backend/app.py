506038b38f175fb90a0af61cde0e516243e9c34e
Reevu01 <reevu1214@gmail.com>
2024-10-25 18:26:02 -0500

    jj
    n

--- useless/flask-backend/app.py
+++ useless/flask-backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, redirect
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ CORS(app)  # Enable CORS for all routes
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    return jsonify({'message': 'Hello from Flask!'})
+    return jsonify({'message': 'Hello  from Flask!'})
 
 @app.route('/api/submit', methods=['POST'])
 def submit_data():
